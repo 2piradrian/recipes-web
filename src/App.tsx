605fc0660dc3b5fd75bc6b561cb948b413c84bdb
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-03-03 23:19:36 -0300

    query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Home from "./routes/Home";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<div>
-			<Routes>
-				<Route path="/home" element={<Home />} />
-				<Route path="/" element={<Navigate to="/home" />} />
-				<Route path="*" element={<Navigate to="/home" />} />
-			</Routes>
+			<QueryClientProvider client={queryClient}>
+				<Routes>
+					<Route path="/home" element={<Home />} />
+					<Route path="/" element={<Navigate to="/home" />} />
+					<Route path="*" element={<Navigate to="/home" />} />
+				</Routes>
+			</QueryClientProvider>
 		</div>
 	);
 }
