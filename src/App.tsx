9947742c281b70e05b5d7324a9b5adddeb3db168
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-03-03 22:57:49 -0300

    principal routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/home" element={<Home />} />
+				<Route path="/" element={<Navigate to="/home" />} />
+				<Route path="*" element={<Navigate to="/home" />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
