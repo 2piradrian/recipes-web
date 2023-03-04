446ecbd33880d88fdc65a6fee0a287e04b18f679
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-03-04 19:09:02 -0300

    Update Sidebar.tsx

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,31 +1,16 @@
+import { useState } from "react";
 import style from "./style.module.css";
 /* assets */
 import nouser from "../../assets/nouser.jpg";
-import arrowright from "../../assets/arrow-right.svg";
 import home from "../../assets/home.svg";
 
 function Sidebar() {
+	const [state, setState] = useState<boolean>(false);
+
 	return (
 		<div className={style.container}>
 			<nav className={style.sidebar}>
-				<img src={arrowright} alt="arrow" className={style.arrowicon} />
 				<div className={style.routes}>
-					<div className={style.route}>
-						<img src={home} alt="home" />
-						<p>Home</p>
-					</div>
-					<div className={style.route}>
-						<img src={home} alt="home" />
-						<p>Home</p>
-					</div>
-					<div className={style.route}>
-						<img src={home} alt="home" />
-						<p>Home</p>
-					</div>
-					<div className={style.route}>
-						<img src={home} alt="home" />
-						<p>Home</p>
-					</div>
 					<div className={style.route}>
 						<img src={home} alt="home" />
 						<p>Home</p>
