6101c5462578fe647b7315f8f4da448e38ba7801
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-03-04 21:30:13 -0300

    sidebar icon open/close

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,16 +3,20 @@ import style from "./style.module.css";
 /* assets */
 import nouser from "../../assets/nouser.jpg";
 import home from "../../assets/home.svg";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Sidebar() {
 	const [state, setState] = useState<boolean>(false);
 
 	return (
-		<div
-			className={style.container}
-			style={{ width: `${state ? "220px" : "50px"}` }}
-			onClick={() => setState(!state)}>
+		<div className={style.container} style={{ width: `${state ? "220px" : "50px"}` }}>
 			<nav className={style.sidebar}>
+				{state ? (
+					<AiOutlineClose onClick={() => setState(!state)} />
+				) : (
+					<RxHamburgerMenu onClick={() => setState(!state)} />
+				)}
 				<div className={style.routes}>
 					<div className={style.route}>
 						<img src={home} alt="home" />
