a7907fcab8cd665477fca575f0eaf2c58f37d112
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-03-18 17:18:03 -0300

    search bar title

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -3,23 +3,20 @@ import { categories } from "../../data/data";
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { filter_category, no_filters } from "../../redux/actions/actions";
+import Titles from "../titles/Titles";
 
 function SearchBar() {
-	const [filter, setFilter] = useState<string | null>("");
+	const [filter, setFilter] = useState<string | null>();
 
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		/* handleScroll(); */
 		dispatch(filter ? filter_category(filter) : no_filters());
 	}, [filter]);
 
 	return (
 		<div className={style.container}>
-			<form className="form">
-				<label>Búsqueda</label>
-				<input type="text" placeholder="Hamburguesas" />
-			</form>
+			<Titles title="Explora" subtitle="las recetas de la comunidad" />
 			<div className={style.categories}>
 				<div
 					className={style.categoryContainer}
